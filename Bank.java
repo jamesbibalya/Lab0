f751665a8b57f5f9a159b51ffba615e702822739
jamesbib <jamesbibalya2015@gmail.com>
2017-12-06 20:13:07 +0300

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Banking;
+
+/**
+ *
+ * @author 93343
+ */
+public interface Bank {
+    
+    public void Deposit();
+    public void Withdraw();
+    public float getbalance();
+}
---
+++ BankAccount.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Banking;
+
+/**
+ *
+ * @author 93343
+ */
+public class BankAccount implements Bank{
+
+    @Override
+    public void Deposit() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void Withdraw() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public float getbalance() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
+
